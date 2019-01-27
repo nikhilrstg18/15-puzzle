137f9c670c8bd97062c3127ffcf950e822a60c7d
Nikhil Rustagi <nikhil_rustagi@dellteam.com>
2019-01-27 19:10:28 +0530

    minor fix

---
+++ docs/precache-manifest.6a3f5efc0deefe7c27903ec70c83f35b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6078883dbe333bf38113",
+    "url": "/15-puzzle/static/js/runtime~main.6078883d.js"
+  },
+  {
+    "revision": "ebc4af599016ec248e17",
+    "url": "/15-puzzle/static/js/main.ebc4af59.chunk.js"
+  },
+  {
+    "revision": "a737e7bd936da5df95de",
+    "url": "/15-puzzle/static/js/1.a737e7bd.chunk.js"
+  },
+  {
+    "revision": "ebc4af599016ec248e17",
+    "url": "/15-puzzle/static/css/main.f3650a0d.chunk.css"
+  },
+  {
+    "revision": "8f752e1054f8ac41c808b1867cd4e089",
+    "url": "/15-puzzle/index.html"
+  }
+];
